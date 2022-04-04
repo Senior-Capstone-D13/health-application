f41333e372883148047564b5a6ce80c3599af4ef
Saptarshi Mondal <saptarshimondal@utexas.edu>
2022-04-04 10:46:09 -0500

    caloriecalculator and comment

---
+++ health-application-location-service/health-application-location-service/Location_Risk_Reduction/app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -0,0 +1,242 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest.permission.ACTIVITY_RECOGNITION
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.myapplication.databinding.ActivityMapsBinding
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
+    var initialStepCount = -1
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    var calories_burnt = 0
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("TAG", "onAccuracyChanged: Sensor: $sensor; accuracy: $accuracy")
+    }
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        sensorEvent ?: return
+        sensorEvent.values.firstOrNull()?.let {
+            if (initialStepCount == -1) {
+                initialStepCount = it.toInt()
+            }
+            val currentNumberOfStepCount = it.toInt() - initialStepCount
+            calories_burnt = 0.04 * currentNumberOfStepCount
+            updateAllDisplayText(currentNumberOfStepCount,0f, calories_burnt)
+            Log.d("TAG", "Steps count: $currentNumberOfStepCount ")
+        }
+    }
+    val locationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+            locationResult ?: return
+            locationResult.locations.forEach {
+                Log.d("TAG", "New location got: (${it.latitude}, ${it.longitude})")
+            }
+        }
+    }
+    @AfterPermissionGranted(REQUEST_CODE_FINE_LOCATION)
+    private fun setupLocationChangeListener() {
+        if (EasyPermissions.hasPermissions(this, ACCESS_FINE_LOCATION)) {
+            val locationRequest = LocationRequest()
+            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            locationRequest.interval = 5000 // 5000ms (5s)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        } else {
+            EasyPermissions.requestPermissions(
+                host = this,
+                rationale = "For showing your current location on the map.",
+                requestCode = REQUEST_CODE_FINE_LOCATION,
+                perms = *arrayOf(ACCESS_FINE_LOCATION)
+            )
+        }
+    }
+
+    private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var mMap: GoogleMap
+
+    companion object {
+        private const val KEY_SHARED_PREFERENCE = "com.rwRunTrackingApp.sharedPreferences"
+        private const val KEY_IS_TRACKING = "com.rwRunTrackingApp.isTracking"
+
+        private const val REQUEST_CODE_FINE_LOCATION = 1
+        private const val REQUEST_CODE_ACTIVITY_RECOGNITION = 2
+    }
+    fun setupStepCounterListener() {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        stepCounterSensor ?: return
+        sensorManager.registerListener(this@MapsActivity, stepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    private var isTracking: Boolean
+        get() = this.getSharedPreferences(KEY_SHARED_PREFERENCE, Context.MODE_PRIVATE).getBoolean(
+            KEY_IS_TRACKING, false)
+        set(value) = this.getSharedPreferences(KEY_SHARED_PREFERENCE, Context.MODE_PRIVATE).edit().putBoolean(
+            KEY_IS_TRACKING, value).apply()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.startButton.setOnClickListener {
+            mMap.clear()
+
+            isTracking = true
+            updateButtonStatus()
+
+            updateAllDisplayText(0, 0f)
+
+            startTracking()
+        }
+        binding.endButton.setOnClickListener { endButtonClicked() }
+
+        updateButtonStatus()
+
+        if (isTracking) {
+            startTracking()
+        }
+    }
+
+    private fun updateButtonStatus() {
+        binding.startButton.isEnabled = !isTracking
+        binding.endButton.isEnabled = isTracking
+    }
+
+    private fun updateAllDisplayText(stepCount: Int, totalDistanceTravelled: Float, calories_burnt : Float) {
+        binding.numberOfStepTextView.text =  String.format("Step count: %d", stepCount)
+        binding.totalDistanceTextView.text = String.format("Total distance: %.2fm", totalDistanceTravelled)
+        binding.totalCaloriesburntTextView.text = String.format("Total calories burnt: %.2fm", calories_burnt)
+        binding.commentTextView.text = String.format("Keep up the good work")
+
+
+        val averagePace = if (stepCount != 0) totalDistanceTravelled / stepCount.toDouble() else 0.0
+        binding.averagePaceTextView.text = String.format("Average pace: %.2fm/ step", averagePace)
+    }
+
+    private fun endButtonClicked() {
+        AlertDialog.Builder(this)
+            .setTitle("Are you sure to stop tracking?")
+            .setPositiveButton("Confirm") { _, _ ->
+                isTracking = false
+                updateButtonStatus()
+                stopTracking()
+            }.setNegativeButton("Cancel") { _, _ ->
+            }
+            .create()
+            .show()
+    }
+
+    @SuppressLint("CheckResult")
+    private fun startTracking() {
+        val isActivityRecognitionPermissionFree = Build.VERSION.SDK_INT < Build.VERSION_CODES.Q
+        val isActivityRecognitionPermissionGranted = EasyPermissions.hasPermissions(this,
+            ACTIVITY_RECOGNITION)
+        Log.d("TAG", "Is ACTIVITY_RECOGNITION permission granted $isActivityRecognitionPermissionGranted")
+        if (isActivityRecognitionPermissionFree || isActivityRecognitionPermissionGranted) {
+            setupStepCounterListener()
+            setupLocationChangeListener()
+        } else {
+            EasyPermissions.requestPermissions(
+                host = this,
+                rationale = "For showing your step counts and calculate the average pace.",
+                requestCode = REQUEST_CODE_ACTIVITY_RECOGNITION,
+                perms = *arrayOf(ACTIVITY_RECOGNITION)
+            )
+        }
+    }
+
+    private fun stopTracking() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        sensorManager.unregisterListener(this, stepCounterSensor)
+    }
+
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        showUserLocation()
+    }
+    @AfterPermissionGranted(REQUEST_CODE_FINE_LOCATION)
+    private fun showUserLocation() {
+        if (EasyPermissions.hasPermissions(this, ACCESS_FINE_LOCATION)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            mMap.isMyLocationEnabled = true
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(
+                host = this,
+                rationale = "For showing your current location on the map.",
+                requestCode = REQUEST_CODE_FINE_LOCATION,
+                perms = *arrayOf(ACCESS_FINE_LOCATION)
+            )
+        }
+    }
+
+}
