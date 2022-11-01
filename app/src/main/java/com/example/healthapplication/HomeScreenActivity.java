756df3040d56a416f7be9b7d1b6a0eaf6c177b78
Mona <jflotildes@utexas.edu>
2022-10-31 22:51:36 -0500

    popup window fix

--- app/src/main/java/com/example/healthapplication/HomeScreenActivity.java
+++ app/src/main/java/com/example/healthapplication/HomeScreenActivity.java
@@ -4,12 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -44,66 +48,87 @@ public class HomeScreenActivity extends AppCompatActivity {
         //Popup menu stuff
         homeScreen = (ConstraintLayout) findViewById(R.id.home_screen);
         Button popupUserInformation = findViewById(R.id.popupUserInformation);
+   //     Animation zoomIn = AnimationUtils.loadAnimation(this,R.anim.zoom_in);
+   //     Animation popupButtonClicked = AnimationUtils.loadAnimation(this,R.anim.pop_up_zoom_in);
+
+
+
         popupUserInformation.setText(account.getGivenName());
-        popupUserInformation.setOnClickListener(new View.OnClickListener() {
+
+        //Menu stuff
+        LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        View container = (View) layoutInflater.inflate(
+                R.layout.activity_home_screen_popup_user_information,
+                null);
+
+
+        int xStart = 800;
+        int windowHeight = 1600;
+
+        PopupWindow window = new PopupWindow(container,xStart,windowHeight,true);
+
+        window.getContentView().setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //Menu stuff
-                LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-                ViewGroup container = (ViewGroup) layoutInflater.inflate(
-                        R.layout.activity_home_screen_popup_user_information,
-                        null);
-
-
-                int xStart = 800;
-                int height = 1200;
-                PopupWindow window = new PopupWindow(container,xStart,height,true);
-
-
-                window.setAnimationStyle(-1);//-1 default
-
-                //CENTER WINDOW
-                window.showAtLocation(homeScreen, Gravity.CENTER,0,0);
-
-                //TextViews Inside Menu
-                TextView popupName = window.getContentView().findViewById(R.id.homeScreenName);
-                TextView popupAge = window.getContentView().findViewById(R.id.age);
-                TextView popupEmail = window.getContentView().findViewById(R.id.email);
-                TextView popupHeight = window.getContentView().findViewById(R.id.height);
-                popupUserInformation.setText(account.getGivenName());
-
-                popupName.setText("Name " + account.getGivenName());
-                popupAge.setText("Age: " + age);
-                popupEmail.setText("Email: " + email);
-                popupHeight.setText("Height: " + height + " inches");
-
-                //TextView textview_age = findViewById(R.id.homeScreenName);
-                //textview_age.setText("Age: " + age + "\n" + "Email: " + email + "\n" + "height: " + height + "inches");
-
-                //Buttons inside menu
-                Button signOut = window.getContentView().findViewById(R.id.signout);
-                signOut.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        mGoogleSignInClient.signOut().addOnCompleteListener(HomeScreenActivity.this, new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                finish();
-                                startActivity(new Intent(HomeScreenActivity.this, MainActivity.class));
-                            }
-                        });
-                    }
-
-                });
-
+            public void onClick(View v) {
+                window.dismiss();
+                Log.d("window", "dismissed");
             }
         });
 
+        //TextViews Inside Menu
+        TextView popupName = window.getContentView().findViewById(R.id.homeScreenName);
+        TextView popupAge = window.getContentView().findViewById(R.id.age);
+        TextView popupEmail = window.getContentView().findViewById(R.id.email);
+        TextView popupHeight = window.getContentView().findViewById(R.id.height);
+        popupUserInformation.setText(account.getGivenName());
+
+        popupName.setText("Name " + account.getGivenName());
+        popupAge.setText("Age: " + age);
+        popupEmail.setText("Email: " + email);
+        popupHeight.setText("Height: " + height + " inches");
+
+
+        //TextView textview_age = findViewById(R.id.homeScreenName);
+        //textview_age.setText("Age: " + age + "\n" + "Email: " + email + "\n" + "height: " + height + "inches");
+        //Buttons inside menu
+        Button signOut = window.getContentView().findViewById(R.id.signout);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGoogleSignInClient.signOut().addOnCompleteListener(HomeScreenActivity.this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        window.dismiss();
+                        finish();
+                        startActivity(new Intent(HomeScreenActivity.this, MainActivity.class));
+                    }
+                });
+            }
+
+        });
+
+        popupUserInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Animation of Button
+//                popupUserInformation.startAnimation(popupButtonClicked);
+
+                //Enter animation of popup window
+  //              container.startAnimation(zoomIn);
+                //CENTER WINDOW
+                window.showAtLocation(homeScreen, Gravity.CENTER,0,0);
+                window.showAsDropDown(view);
+           }
+        });
+
         Button goToChallengeFragments = findViewById(R.id.goChallengeMain);
+ //       Animation effect = AnimationUtils.loadAnimation(this,R.anim.slide_out_right);
         goToChallengeFragments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                v.startAnimation(effect);
                 startActivity(new Intent(HomeScreenActivity.this, ChallengesActivityMain.class));
+//                overridePendingTransition(R.anim.zoom_in,R.anim.expand_effect);
             }
         });
     }
